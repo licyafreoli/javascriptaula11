76f09ae680902afcb0e66dc96f6cf11e0da1db8f
licyafreoli <168486966+licyafreoli@users.noreply.github.com>
2024-07-15 15:38:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ const numeros = [50, 45, 67, 89, 10,15];
+
+const MultiplicarPorDois = numeros.map (numero => numero* 2);
+
+
+console.log("cada número multiplicado por dois:",MultiplicarPorDois)
+// console.log("Array original:", numeros);
+// console.log("Soma dos numeros:", soma);
